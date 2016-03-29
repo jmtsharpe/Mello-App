54992cb17128dd7a73dee89047a2d9091934bb13
James <jmtsharpe@gmail.com>
2016-03-29 17:07:28 -0400

    Allow users to create accounts, log in, and log out

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root to: 'static_pages#root'
+
+  resources :users
+  resource :session
 end
