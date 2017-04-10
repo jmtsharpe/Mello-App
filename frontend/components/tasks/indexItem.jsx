3f2005b0c97668aea1a164819ea39f32df6224c6
James Sharpe <jmtsharpe@gmail.com>
2017-04-10 16:22:32 -0400

    remove commented out code and fix tabbing in tasks/indexItem.jsx

--- frontend/components/tasks/indexItem.jsx
+++ frontend/components/tasks/indexItem.jsx
@@ -8,49 +8,6 @@ var App = require('./../app/app');
 var TaskStore = require('./../../stores/task');
 CardStore = require('./../../stores/card');
 
-// var flow = require('lodash.flow');
-// var DropTarget = require('react-dnd').DropTarget;
-// var PropTypes = React.PropTypes;
-// var DragSource = require('react-dnd').DragSource;
-
-
-// var TaskSource = {
-// 	beginDrag: function (props) {
-//     return {
-//       position: props.task.id,
-// 			id: props.task.id,
-//       subject: props.task.subject,
-// 			cardId: props.task.card_id
-//     };
-//   }
-// };
-//
-// var TaskTarget = {
-// 	hover: function (props, monitor, component) {
-//
-// 	},
-//
-//   drop: function (props, monitor, component) {
-// 		var newPos = component.props.task.id;
-//     // ApiUtil.updateTaskPosition(monitor.getItem(), newPos);
-//     return {};
-//   },
-//
-// };
-//
-// function collect(connect, monitor) {
-//   return {
-//     connectDragSource: connect.dragSource(),
-//     isDragging: monitor.isDragging()
-//   };
-// }
-//
-// function collectTarget(connect, monitor) {
-//   return {
-//     connectDropTarget: connect.dropTarget()
-//   };
-// }
-
 var TaskIndexItem = React.createClass({
 	mixins: [OnClickOutside, LinkedStateMixin],
 
@@ -58,47 +15,40 @@ var TaskIndexItem = React.createClass({
 		router: React.PropTypes.object.isRequired
 	},
 
-	// propTypes: {
-	// 	connectDragSource: React.PropTypes.func.isRequired,
-	// 	isDragging: React.PropTypes.bool.isRequired
-	// },
-
-  getInitialState: function () {
-    return {
+	getInitialState: function () {
+		return ({
 			pressed: false,
 			subject: this.props.task.subject,
 			task: this.props.task
-		};
-  },
+		});
+	},
 
-  isPressed: function () {
+	isPressed: function () {
 		this.setState({pressed: !this.state.pressed});
 	},
 
-  handleClickOutside: function (e) {
-    this.setState({ pressed: false});
-  },
+	handleClickOutside: function (e) {
+		this.setState({ pressed: false});
+	},
 
 	editTask: function (event) {
 
-    event.preventDefault();
-    var task = {};
-    Object.keys(this.state).forEach(function (key) {
-      { task[key] = this.state[key]; }
-    }.bind(this));
-    task.id = this.state.task.id;
-    ApiUtil.editTask(task, this.props.task);
-    this.setState({ pressed: false } );
-  },
+		event.preventDefault();
 
-  render: function () {
+		var task = {};
+		Object.keys(this.state).forEach(function (key) {
+			{ task[key] = this.state[key]; }
+		}.bind(this));
 
-		// var connectDragSource = this.props.connectDragSource;
-    // var isDragging = this.props.isDragging;
-    // var connectDropTarget = this.props.connectDropTarget;
+		task.id = this.state.task.id;
+		ApiUtil.editTask(task, this.props.task);
+
+		this.setState({ pressed: false } );
+	},
+
+	render: function () {
 
 		if (!this.state.pressed) {
-			// connectDragSource(connectDropTarget
 			return (
 				<div className="task-list-padding">
 					<div className="task-list-item" onClick={this.isPressed}>
@@ -107,34 +57,29 @@ var TaskIndexItem = React.createClass({
 				</div>
 			);
 		}
-		// connectDragSource(connectDropTarget
-    return (
-      <div className="task-list-padding" >
-        <div className="task-list-item" onClick={this.isPressed}>
-          <p>{this.props.task.subject}</p>
-        </div>
+
+		return (
+			<div className="task-list-padding" >
+				<div className="task-list-item" onClick={this.isPressed}>
+					<p>{this.props.task.subject}</p>
+				</div>
 				<div className="overlay-back" onClick={this.isPressed} />
 				<div className="edit-task">
-	        <form className="task-edit-form" onSubmit={this.editTask}>
+					<form className="task-edit-form" onSubmit={this.editTask}>
 						<h3 className="edit-task-head">Edit Task</h3>
-	          <textarea
-	            className="task-form-field"
-	            type='text'
-	            id='task_subject'
-	            valueLink={this.linkState("subject")}
-	          />
-	          <br />
-	  				<button className="submit">Save</button>
-	        </form>
-	      </div>
-      </div>
-    );
-  }
+						<textarea
+							className="task-form-field"
+							type='text'
+							id='task_subject'
+							valueLink={this.linkState("subject")}
+						/>
+						<br />
+						<button className="submit">Save</button>
+					</form>
+				</div>
+			</div>
+		);
+	}
 });
-
-// module.exports = flow(
-//   DragSource("indexItem", TaskSource, collect),
-//   DropTarget("indexItem", TaskTarget, collectTarget)
-// )(TaskIndexItem);
 
 module.exports = TaskIndexItem;
